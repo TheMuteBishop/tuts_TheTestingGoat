1922a3f14259b3bf48ed4850226061c6c68b6529
TheMuteBishop <shivendrabind@yahoo.com>
2018-07-22 02:19:01 +0530

    home view returns minimal html

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,22 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
 
-class SmokeTest(TestCase):
+from lists.views import home_view
 
-    def test_bad_math(self):
-        # this test should fail
-        self.assertIn(2+2 , 3)
+class HomePageTest(TestCase):
 
+    # def test_bad_math(self):
+    #     # this test should fail
+    #     self.assertIn(2+2 , 3)
+
+    def test_root_url_to_home_view(self):
+        home = resolve('/')
+        self.assertEqual(home.func, home_view)
+
+    def test_home_view_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_view(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'To-Do' , response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
